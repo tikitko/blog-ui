bf6aac0972126e4f9e6708a0b5ad9dd9a6a0070f
tikitko <bns.6587@gmail.com>
2023-05-25 07:25:13 +0000

    Deploying to gh-pages from @ tikitko/blog@3702581317a637adea9d89a11d20faa2f62c0d6a 🚀

--- blog-f1d13219487962cc.js
+++ blog-f1d13219487962cc.js
@@ -941,15 +941,15 @@ function __wbg_get_imports() {
     imports.wbg.__wbg_removeAttribute_77e4f460fd0fde34 = function() { return handleError(function (arg0, arg1, arg2) {
         getObject(arg0).removeAttribute(getStringFromWasm0(arg1, arg2));
     }, arguments) };
-    imports.wbg.__wbindgen_closure_wrapper647 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper690 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 49, __wbg_adapter_36);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper2263 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper2277 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 69, __wbg_adapter_39);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper3227 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper3238 = function(arg0, arg1, arg2) {
         const ret = makeClosure(arg0, arg1, 69, __wbg_adapter_42);
         return addHeapObject(ret);
     };
@@ -993,7 +993,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('blog-c4e7ad143b884af9_bg.wasm', import.meta.url);
+        input = new URL('blog-f1d13219487962cc_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
