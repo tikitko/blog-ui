886406d36f9322063434519d5d8b5fc4897b2754
tikitko <bns.6587@gmail.com>
2023-05-27 06:11:44 +0000

    Deploying to gh-pages from @ tikitko/blog@0829724fad7b3d81ba695cb6f69267381e559819 🚀

--- blog-67260b6e0fe06743.js
+++ blog-67260b6e0fe06743.js
@@ -968,7 +968,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('blog-a4f6778a38e9576c_bg.wasm', import.meta.url);
+        input = new URL('blog-67260b6e0fe06743_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
